4f1c912b1e7884f197cee80f6e89d3864d92e758
sudipto <sudiptomahato@gmail.com>
2015-03-21 14:04:27 +0530

    infinite load upload
    file upload

---
+++ infinitescroll.js
@@ -0,0 +1,53 @@
+(function(a) {
+    function h() {
+        g || (g = !0, d ? (b.find("a").hide(), b.find("img").show(), a.ajax(d, {
+            dataType: "html"
+        }).done(function(c) {
+            var c = a("<div></div>").append(c.replace(j, "")),
+                e = c.find("a.blog-pager-older-link");
+            //e ? d = e.attr("href") : (d = "", b.hide());
+             c = c.find(i).children();
+            if(e&&2!=c.length)
+            {
+            	d = e.attr("href");
+            	//alert(c.length);
+            }else{
+            	d = "";
+            	b.hide();
+            }
+           // c = c.find(i).children();
+            a(i).append(c);
+            window._gaq && window._gaq.push(["_trackPageview", d]);
+            b.find("img").hide();
+            b.find("a").show();
+            g = !1
+        })) : b.hide())
+    }
+
+    function k() {
+        var a = Math.max(f.height(), l.height(), document.documentElement.clientHeight),
+            b = f.scrollTop() + f.height();
+        150 > a - b && h()
+    }
+    var d = "",
+        b = null,
+        i = "div.blog-posts",
+        g = !1,
+        f = a(window),
+        l = a(document),
+        j = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
+    a(document).ready(function() {
+        if ("item" != _WidgetManager._GetAllData().blog.pageType && (d = a("a.blog-pager-older-link").attr("href"))) {
+            var c =
+                a('<a href="javascript:;" class="loadmp">Load more posts</a>');
+            c.click(h);
+            var e = a('<img src="https://lh3.googleusercontent.com/-FiCzyOK4Mew/T4aAj2uVJKI/AAAAAAAAPaY/x23tjGIH7ls/s32/ajax-loader.gif" style="display: none;">');
+            f.scroll(k);
+            b = a('<div style="text-align: center; font-size: 150%;"></div>');
+            b.append(c);
+            b.append(e);
+            b.insertBefore(a("#blog-pager"));
+            a("#blog-pager").hide()
+        }
+    })
+})(jQuery);
